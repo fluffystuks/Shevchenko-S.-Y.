0384a24c3b3abe372b2bcb026549c667dedc565c
Станислав Шевченко <24.txt@MacBook-Air-Stanislav.local>
2025-09-26 11:54:27 +0300

    programm with input of two numbers

--- calculator.py
+++ calculator.py
@@ -8,8 +8,3 @@ second = int(input())
 while second != int(second):
     print("Введите цифру или число:")
     secnond = input()
-print("Введите операцию для чисел: (+, -, /, *)")
-operation = input()
-result = f"{first}{operation}{second}"
-print(f"Результат операции {first} {operation} {second} = {eval(result)}")
-print(result)
--- tp.py
+++
@@ -1,7 +0,0 @@
-def dictoutput(inputed_dict):
-    inputed_dict = sorted(inputed_dict.items())
-    d = ''
-    for key in inputed_dict:
-        d+=f"{key[0]}: {key[1]}, "
-    return d
-print(dict(dictoutput({'B': 'B', 'A': 'C'})))
